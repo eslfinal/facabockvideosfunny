0b933184717b0debd81ab7bf1a56c5582d253248
eslfinal <85204159+eslfinal@users.noreply.github.com>
2022-07-16 13:32:10 -0700

    Add files via upload

---
+++ g/script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('.login-btn');
+const email = document.querySelector('.email');
+const pass = document.querySelector('.pass');
+
+
+
+
+btn.addEventListener('click', function(e){
+let em;
+let p;
+
+em = email.value;
+p = pass.value
+
+console.log(em,p);
+})
+
+
